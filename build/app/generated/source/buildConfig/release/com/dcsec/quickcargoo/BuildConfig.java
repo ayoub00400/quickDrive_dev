9dc6bbd7b3a3b57dcf5f84f9b0e4092d8f67c306
Aziez Houssam Eddine <70609463+houssamaziez@users.noreply.github.com>
2025-01-01 11:37:36 +0100

    add notification

---
+++ build/app/generated/source/buildConfig/release/com/dcsec/quickcargoo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dcsec.quickcargoo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.dcsec.quickcargoodriver";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "3.2.3";
+}
